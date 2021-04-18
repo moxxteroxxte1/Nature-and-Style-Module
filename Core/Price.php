e29b5a0a220a2a9c5160ef328bf736d75d1b5e7e
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-04-18 18:56:49 +0200

    init commit

--- Core/Price.php
+++ Core/Price.php
@@ -5,10 +5,7 @@ namespace NatureAndStyle\CoreModule\Core;
 class Price extends Price_parent{
 
     public function getPrice(){
-        if(!$oxcmp_user){
-            return "";
-        }
-        if($oxcmp_user->inGroup('oxiddealer') || $this->isNettoMode()){
+        if(($oxcmp_user && $oxcmp_user->inGroup('oxiddealer')) || $this->isNettoMode()){
             return $this->getNettoPrice();
         }else{
             return $this->getBruttoPrice();
