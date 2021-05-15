4b836036929ab3ed10c792966c00b89251df9c0e
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-05-15 19:41:10 +0200

    added services.yaml and edited metadata.php

--- Application/Model/Basket.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace NatureAndStyle\CoreModule\Application\Model;
-
-class Basket extends Basket_parent
-{
-
-}
--- metadata.php
+++ metadata.php
@@ -21,9 +21,9 @@ $aModule = array(
     'url'           => 'http://www.natureandstyle.com/',
     'email'         => 'info@natureandstyle.de',
     'extend'        => array(
-        \OxidEsales\GdprOptinModule\Component\UserComponent::class                => \NatureAndStyle\CoreModule\Application\Component\UserComponent::class,
+        \OxidEsales\GdprOptinModule\Component\UserComponent::class                  => \NatureAndStyle\CoreModule\Application\Component\UserComponent::class,
         \OxidEsales\Eshop\Application\Model\User::class                             => \NatureAndStyle\CoreModule\Application\Model\User::class,
-        \OxidEsales\Eshop\Application\Model\Basket::class                           => \NatureAndStyle\CoreModule\Application\Model\Basket::class,
+        \OxidEsales\Eshop\Application\Model\Discount::class                         => \NatureAndStyle\CoreModule\Application\Model\Discount::class,
         \OxidEsales\Eshop\Core\Price::class                                         => \NatureAndStyle\CoreModule\Core\Price::class,
     ),
     'events'       => array(
@@ -33,12 +33,22 @@ $aModule = array(
         array(
             'template'  => 'article_pictures.tpl',
             'block'     => 'admin_article_pictures_main',
-            'file'      => '/views/blocks/article_pictures_extend.tpl',
+            'file'      => '/Application/views/blocks/article_pictures_extend.tpl',
         ),
         array(
             'template'  => 'user_main.tpl',
             'block'     => 'admin_user_main_assign_groups',
-            'file'      => '/views/blocks/user_main.tpl',
+            'file'      => '/Application/views/blocks/user_main_extend.tpl',
+        ),
+        array(
+            'template'  => 'article_main.tpl',
+            'block'     => 'admin_article_main_extended',
+            'file'      => '/Application/views/blocks/article_main_extend.tpl',
+        ),
+        array(
+            'template'  => 'discount_main.tpl',
+            'block'     => 'admin_discount_main_form',
+            'file'      => '/Application/views/blocks/discount_main_extend.tpl',
         ),
     ),
 );
