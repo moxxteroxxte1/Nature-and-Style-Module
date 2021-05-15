a274a52cf504205bd38c7cc26e98c77244211b4b
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-05-15 19:56:36 +0200

    Added translation for oxinactive

--- translations/de/nascore_lang_de.php
+++ translations/de/nascore_lang_de.php
@@ -6,5 +6,5 @@ $aLang = array(
     'charset' => 'UTF-8',
     'SHOP_MODULE_GROUP_price' => 'Preise',
     'SHOP_MODULE_sGroupNetto' => 'Benutzergruppe, der die Nettopreise angezeigt werden sollen.',
-
+    'oxinactive'              => 'Warten auf Freigabe',
 );
--- translations/en/nascore_lang_en.php
+++ translations/en/nascore_lang_en.php
@@ -6,5 +6,5 @@ $aLang = array(
     'charset' => 'UTF-8',
     'SHOP_MODULE_GROUP_price' => 'Prices',
     'SHOP_MODULE_sGroupNetto' => 'User group to which net prices should be displayed',
-
+    'oxinactive'              => 'Waiting for approval',
 );
