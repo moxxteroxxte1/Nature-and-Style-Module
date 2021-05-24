87312609459cc816f85c2efbb5c598465216162f
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-05-24 15:27:58 +0200

    added some debugprints

--- Application/Model/Article.php
+++ Application/Model/Article.php
@@ -2,21 +2,16 @@
 
 namespace NatureAndStyle\CoreModule\Application\Model;
 
+use OxidEsales\Eshop\Application\Model\DiscountList;
+use OxidEsales\Eshop\Core\DatabaseProvider;
+use OxidEsales\Eshop\Core\Registry;
+
 class Article extends Article_parent
 {
 
-    public function hasPackagingUnitDiscount(){
-       $user = \OxidEsales\Eshop\Core\Registry::getSession()->getUser();
-       if($user){
-           $aDiscounts = new \OxidEsales\Eshop\Application\Model\DiscountList();
-           $aDiscounts->getArticleDiscounts($this, $user);
-           foreach ($aDiscounts as $oDiscount){
-               if($oDiscount->oxdiscount__oxamountpackageunit->value){
-                   return $oDiscount->oxdiscounts__oxtitle->value;
-               }
-           }
-       }
-       return false;
+    public function hasPackagingUnitDiscount()
+    {
+        Registry::getLogger()->warning($this->queryFilter());
     }
 
     public function getTitle()
@@ -47,5 +42,61 @@ class Article extends Article_parent
             }
         }
         return false;
+    }
+
+    private function queryFilter(){
+        $aDiscountList = new DiscountList();
+        $oBaseObject = $aDiscountList->getBaseObject();
+
+        $sTable = $oBaseObject->getViewName();
+        $sQ = "select " . $oBaseObject->getSelectFields() . " from $sTable ";
+        $sQ .= "where " . $oBaseObject->getSqlActiveSnippet() . ' ';
+
+
+        // defining initial filter parameters
+        $sUserId = null;
+        $sGroupIds = null;
+        $oUser = Registry::getSession()->getUser();
+        $sCountryId = $this->getCountryId($oUser);
+        $oDb = DatabaseProvider::getDb();
+
+        // checking for current session user which gives additional restrictions for user itself, users group and country
+        if ($oUser) {
+            // user ID
+            $sUserId = $oUser->getId();
+
+            // user group ids
+            foreach ($oUser->getUserGroups() as $oGroup) {
+                if ($sGroupIds) {
+                    $sGroupIds .= ', ';
+                }
+                $sGroupIds .= $oDb->quote($oGroup->getId());
+            }
+        }
+
+        $sUserTable = getViewName('oxuser');
+        $sGroupTable = getViewName('oxgroups');
+        $sCountryTable = getViewName('oxcountry');
+
+        $sCountrySql = $sCountryId ? "EXISTS(select oxobject2discount.oxid from oxobject2discount where oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxcountry' and oxobject2discount.OXOBJECTID=" . $oDb->quote($sCountryId) . ")" : '0';
+        $sUserSql = $sUserId ? "EXISTS(select oxobject2discount.oxid from oxobject2discount where oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxuser' and oxobject2discount.OXOBJECTID=" . $oDb->quote($sUserId) . ")" : '0';
+        $sGroupSql = $sGroupIds ? "EXISTS(select oxobject2discount.oxid from oxobject2discount where oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxgroups' and oxobject2discount.OXOBJECTID in ($sGroupIds) )" : '0';
+
+        $sQ .= "and (
+            select
+                if(EXISTS(select 1 from oxobject2discount, $sCountryTable where $sCountryTable.oxid=oxobject2discount.oxobjectid and oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxcountry' LIMIT 1),
+                        $sCountrySql,
+                        1) &&
+                if(EXISTS(select 1 from oxobject2discount, $sUserTable where $sUserTable.oxid=oxobject2discount.oxobjectid and oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxuser' LIMIT 1),
+                        $sUserSql,
+                        1) &&
+                if(EXISTS(select 1 from oxobject2discount, $sGroupTable where $sGroupTable.oxid=oxobject2discount.oxobjectid and oxobject2discount.OXDISCOUNTID=$sTable.OXID and oxobject2discount.oxtype='oxgroups' LIMIT 1),
+                        $sGroupSql,
+                        1)
+            )";
+
+        $sQ .= " order by $sTable.oxsort ";
+
+        return $sQ;
     }
 }
