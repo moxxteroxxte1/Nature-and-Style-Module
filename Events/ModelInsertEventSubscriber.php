17919dce4b1f204d4c1b604faab73e2e3a07876a
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-05-26 19:28:10 +0200

    even more fixes

--- Events/ModelInsertEventSubscriber.php
+++ Events/ModelInsertEventSubscriber.php
@@ -50,8 +50,9 @@ class ModelInsertEventSubscriber extends AbstractShopAwareEventSubscriber
 
         if (is_a($model, "OxidEsales\Eshop\Application\Model\Article")) {
             $sArtNum = $model->oxarticles__oxartnum->value;
+
             $model->setId($sArtNum);
-            $model->save();
+
             $this->articleToCategory($sArtNum, $this->category_new);
         }
     }
