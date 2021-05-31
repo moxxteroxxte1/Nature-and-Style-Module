c6471e8964f4d58d1c3069ee0da3360175adbf2d
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-05-31 22:26:14 +0200

    some fixes

--- Application/Model/ActionList.php
+++ Application/Model/ActionList.php
@@ -11,7 +11,29 @@ class ActionList extends ActionList_parent
         $sQ = "select * from {$oViewName} where oxtype=4 and " . $oBaseObject->getSqlActiveSnippet()
             . " and oxshopid='" . \OxidEsales\Eshop\Core\Registry::getConfig()->getShopId() . "' " . $this->_getUserGroupFilter()
             . " order by oxsort";
-        $this->selectString($sQ);
+
+        $this->clear();
+        $oDb = \OxidEsales\Eshop\Core\DatabaseProvider::getDb(\OxidEsales\Eshop\Core\DatabaseProvider::FETCH_MODE_ASSOC);
+        if ($this->_aSqlLimit[0] || $this->_aSqlLimit[1]) {
+            $rs = $oDb->selectLimit($sQ, $this->_aSqlLimit[1], max(0, $this->_aSqlLimit[0]), []);
+        } else {
+            $rs = $oDb->select($sQ, []);
+        }
+
+        if ($rs != false && $rs->count() > 0) {
+            $oSaved = clone oxNew(\NatureAndStyle\CoreModule\Application\Model\Tile::class);
+
+            while (!$rs->EOF) {
+                $oListObject = clone $oSaved;
+
+                $this->_assignElement($oListObject, $rs->fields);
+
+                $this->add($oListObject);
+
+                $rs->fetchRow();
+            }
+        }
     }
+
 
 }
