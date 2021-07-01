e570099a815b6cef4d985afc5ad84a4c807cee20
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-07-01 23:20:57 +0200

    some tests

--- Application/Controller/Admin/UserMain.php
+++ Application/Controller/Admin/UserMain.php
@@ -10,7 +10,12 @@ class UserMain extends UserMain_parent
 {
 
     public function login(){
+        $config = Registry::getConfig();
         $oxid = Registry::getRequest()->getRequestEscapedParameter("oxid");
+
+        $oUser = oxNew('oxuser');
+        $oUser->load($oxid);
+        $oUser->loadAuthenticatedUser($oUser->oxuser__oxusername->value, $config->getShopId());
         Registry::getSession()->setVariable('usr', $oxid);
 
         $sUrl = Registry::getConfig()->getShopHomeUrl();
