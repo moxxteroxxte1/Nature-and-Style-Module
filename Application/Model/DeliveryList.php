e5770966d5b4c7ad7212bdc480fa7f61b9ed88f9
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-07-02 16:13:24 +0200

    order DeliveryList by price

--- Application/Model/DeliveryList.php
+++ Application/Model/DeliveryList.php
@@ -10,7 +10,7 @@ class DeliveryList extends DeliveryList_parent
     public function getDeliveryList($oBasket, $oUser = null, $sDelCountry = null, $sDelSet = null)
     {
         $aDeliveryList = parent::getDeliveryList($oBasket, $oUser, $sDelCountry, $sDelSet);
-        return usort($aDeliveryList, function ($a,$b){return $a-getDeliveryPrice()->getPrice() <=> $b-getDeliveryPrice()->getPrice();});
+        return usort($aDeliveryList, function ($a,$b){return $a->getDeliveryPrice()->getPrice() <=> $b->getDeliveryPrice()->getPrice();});
     }
 
 }
