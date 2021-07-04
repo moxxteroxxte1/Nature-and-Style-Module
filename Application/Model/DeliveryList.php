54122011ff0f559e9f181895e3f4038e9e08b9bb
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-07-04 14:32:11 +0200

    some fixes

--- Application/Model/DeliveryList.php
+++ Application/Model/DeliveryList.php
@@ -67,13 +67,7 @@ class DeliveryList extends DeliveryList_parent
             }
         }
 
-        if ($this->blFindCheapest) {
-            ksort($aUnsortedDeliveries, SORT_NUMERIC);
-            $aDeliverySet = array_shift($aUnsortedDeliveries);
-
-            Registry::getSession()->setVariable('sShipSet', $aDeliverySet['set']);
-            return [$aDeliverySet['delivery']];
-        } elseif ($this->_blCollectFittingDeliveriesSets && count($aFittingDelSets)) {
+        if ($this->_blCollectFittingDeliveriesSets && count($aFittingDelSets)) {
             //resetting getting delivery sets list instead of deliveries before return
             $this->_blCollectFittingDeliveriesSets = false;
 
@@ -84,43 +78,15 @@ class DeliveryList extends DeliveryList_parent
             return $aFittingDelSets;
         }
 
+        if ($this->blFindCheapest) {
+            ksort($aUnsortedDeliveries, SORT_NUMERIC);
+            $aDeliverySet = array_shift($aUnsortedDeliveries);
+
+            Registry::getSession()->setVariable('sShipSet', $aDeliverySet['set']);
+            return [$aDeliverySet['delivery']];
+        }
+
         // nothing what fits was found
         return [];
     }
-
-    private function array_sort($array, $on, $order = SORT_ASC)
-    {
-        $new_array = array();
-        $sortable_array = array();
-
-        if (count($array) > 0) {
-            foreach ($array as $k => $v) {
-                if (is_array($v)) {
-                    foreach ($v as $k2 => $v2) {
-                        if ($k2 == $on) {
-                            $sortable_array[$k] = $v2;
-                        }
-                    }
-                } else {
-                    $sortable_array[$k] = $v;
-                }
-            }
-
-            switch ($order) {
-                case SORT_ASC:
-                    asort($sortable_array);
-                    break;
-                case SORT_DESC:
-                    arsort($sortable_array);
-                    break;
-            }
-
-            foreach ($sortable_array as $k => $v) {
-                $new_array[$k] = $array[$k];
-            }
-        }
-
-        return $new_array;
-    }
-
 }
