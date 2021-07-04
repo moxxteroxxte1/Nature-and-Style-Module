2e3f974f7e6bb237cd9f39693f9c345f73ba5a15
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-07-04 13:20:41 +0200

    some fixes

--- metadata.php
+++ metadata.php
@@ -1,111 +1,136 @@
 <?php
 
+use NatureAndStyle\CoreModule\Application\Component\UserComponent;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsCategoryAjax;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsList;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsMain;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\ArticlePictures;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\ArticleStock;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\DeliveryMain;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\TileController;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\TileList;
+use NatureAndStyle\CoreModule\Application\Controller\Admin\TileMain;
+use NatureAndStyle\CoreModule\Application\Controller\BasketController;
+use NatureAndStyle\CoreModule\Application\Controller\StartController;
+use NatureAndStyle\CoreModule\Application\Model\ActionList;
+use NatureAndStyle\CoreModule\Application\Model\Actions;
+use NatureAndStyle\CoreModule\Application\Model\Article;
+use NatureAndStyle\CoreModule\Application\Model\Basket;
+use NatureAndStyle\CoreModule\Application\Model\BasketItem;
+use NatureAndStyle\CoreModule\Application\Model\Delivery;
+use NatureAndStyle\CoreModule\Application\Model\DeliveryList;
+use NatureAndStyle\CoreModule\Application\Model\Discount;
+use NatureAndStyle\CoreModule\Application\Model\OrderArticle;
+use NatureAndStyle\CoreModule\Application\Model\User;
+use NatureAndStyle\CoreModule\Core\Price;
+
 $sMetadataVersion = '2.0';
 
 /**
  * Module information
  */
 $aModule = array(
-    'id'            => 'nascore',
-    'title'         => [
-        'de'    => 'Nature and Style.',
-        'en'    => 'Nature and Style.',
+    'id' => 'nascore',
+    'title' => [
+        'de' => 'Nature and Style.',
+        'en' => 'Nature and Style.',
     ],
-    'description'   => [
-        'de'    => 'Nature and Style Erweiterung.',
-        'en'    => 'Nature and Style extension.',
+    'description' => [
+        'de' => 'Nature and Style Erweiterung.',
+        'en' => 'Nature and Style extension.',
     ],
-    'thumbnail'     => 'out/pictures/logo.png',
-    'version'       => '1.0.0',
-    'author'        => 'Nature and Style',
-    'url'           => 'https://www.natureandstyle.com/',
-    'email'         => 'info@natureandstyle.de',
-    'extend'        => array(
+    'thumbnail' => 'out/pictures/logo.png',
+    'version' => '1.0.0',
+    'author' => 'Nature and Style',
+    'url' => 'https://www.natureandstyle.com/',
+    'email' => 'info@natureandstyle.de',
+    'extend' => array(
         //Core
-        \OxidEsales\Eshop\Core\Price::class                                         => \NatureAndStyle\CoreModule\Core\Price::class,
+        \OxidEsales\Eshop\Core\Price::class => Price::class,
 
         //Admin Controller
-        \OxidEsales\Eshop\Application\Controller\Admin\ActionsMain::class           => \NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsMain::class,
-        \OxidEsales\Eshop\Application\Controller\Admin\ActionsList::class           => \NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsList::class,
-        \OxidEsales\Eshop\Application\Controller\Admin\DeliveryMain::class          => \NatureAndStyle\CoreModule\Application\Controller\Admin\DeliveryMain::class,
-        \OxidEsales\Eshop\Application\Controller\Admin\ArticleStock::class          => \NatureAndStyle\CoreModule\Application\Controller\Admin\ArticleStock::class,
-        \OxidEsales\Eshop\Application\Controller\Admin\ArticlePictures::class       => \NatureAndStyle\CoreModule\Application\Controller\Admin\ArticlePictures::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\ActionsMain::class => ActionsMain::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\ActionsList::class => ActionsList::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\DeliveryMain::class => DeliveryMain::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\ArticleStock::class => ArticleStock::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\ArticlePictures::class => ArticlePictures::class,
 
         //Controller
-        \OxidEsales\Eshop\Application\Controller\StartController::class             => \NatureAndStyle\CoreModule\Application\Controller\StartController::class,
-        \OxidEsales\Eshop\Application\Controller\BasketController::class            => \NatureAndStyle\CoreModule\Application\Controller\BasketController::class,
+        \OxidEsales\Eshop\Application\Controller\StartController::class => StartController::class,
+        \OxidEsales\Eshop\Application\Controller\BasketController::class => BasketController::class,
 
         //Components
-        \OxidEsales\Eshop\Application\Component\UserComponent::class                => \NatureAndStyle\CoreModule\Application\Component\UserComponent::class,
+        \OxidEsales\Eshop\Application\Component\UserComponent::class => UserComponent::class,
 
         //Models
-        \OxidEsales\Eshop\Application\Model\User::class                             => \NatureAndStyle\CoreModule\Application\Model\User::class,
-        \OxidEsales\Eshop\Application\Model\OrderArticle::class                     => \NatureAndStyle\CoreModule\Application\Model\OrderArticle::class,
-        \OxidEsales\Eshop\Application\Model\Article::class                          => \NatureAndStyle\CoreModule\Application\Model\Article::class,
-        \OxidEsales\Eshop\Application\Model\Discount::class                         => \NatureAndStyle\CoreModule\Application\Model\Discount::class,
-        \OxidEsales\Eshop\Application\Model\Actions::class                          => \NatureAndStyle\CoreModule\Application\Model\Actions::class,
-        \OxidEsales\Eshop\Application\Model\ActionList::class                       => \NatureAndStyle\CoreModule\Application\Model\ActionList::class,
-        \OxidEsales\Eshop\Application\Model\Delivery::class                         => \NatureAndStyle\CoreModule\Application\Model\Delivery::class,
-        \OxidEsales\Eshop\Application\Model\Basket::class                           => \NatureAndStyle\CoreModule\Application\Model\Basket::class,
-        \OxidEsales\Eshop\Application\Model\BasketItem::class                       => \NatureAndStyle\CoreModule\Application\Model\BasketItem::class,
+        \OxidEsales\Eshop\Application\Model\User::class => User::class,
+        \OxidEsales\Eshop\Application\Model\OrderArticle::class => OrderArticle::class,
+        \OxidEsales\Eshop\Application\Model\Article::class => Article::class,
+        \OxidEsales\Eshop\Application\Model\Discount::class => Discount::class,
+        \OxidEsales\Eshop\Application\Model\Actions::class => Actions::class,
+        \OxidEsales\Eshop\Application\Model\ActionList::class => ActionList::class,
+        \OxidEsales\Eshop\Application\Model\Delivery::class => Delivery::class,
+        \OxidEsales\Eshop\Application\Model\DeliveryList::class => DeliveryList::class,
+        \OxidEsales\Eshop\Application\Model\Basket::class => Basket::class,
+        \OxidEsales\Eshop\Application\Model\BasketItem::class => BasketItem::class,
     ),
-    'templates'     => array(
-        'actions_category.tpl'  => 'nature-and-style/core-module/Application/views/admin/tpl/popups/actions_category.tpl',
-        'tile_category.tpl'     => 'nature-and-style/core-module/Application/views/admin/tpl/popups/tile_category.tpl',
-        'tile.tpl'              => 'nature-and-style/core-module/Application/views/admin/tpl/tile.tpl',
-        'tile_list.tpl'         => 'nature-and-style/core-module/Application/views/admin/tpl/tile_list.tpl',
-        'tile_main.tpl'         => 'nature-and-style/core-module/Application/views/admin/tpl/tile_main.tpl',
+    'templates' => array(
+        'actions_category.tpl' => 'nature-and-style/core-module/Application/views/admin/tpl/popups/actions_category.tpl',
+        'tile_category.tpl' => 'nature-and-style/core-module/Application/views/admin/tpl/popups/tile_category.tpl',
+        'tile.tpl' => 'nature-and-style/core-module/Application/views/admin/tpl/tile.tpl',
+        'tile_list.tpl' => 'nature-and-style/core-module/Application/views/admin/tpl/tile_list.tpl',
+        'tile_main.tpl' => 'nature-and-style/core-module/Application/views/admin/tpl/tile_main.tpl',
     ),
-    'controllers'   => array(
-        'actions_category_ajax' => \NatureAndStyle\CoreModule\Application\Controller\Admin\ActionsCategoryAjax::class,
-        'tile'                  => \NatureAndStyle\CoreModule\Application\Controller\Admin\TileController::class,
-        'tile_list'             => \NatureAndStyle\CoreModule\Application\Controller\Admin\TileList::class,
-        'tile_main'             => \NatureAndStyle\CoreModule\Application\Controller\Admin\TileMain::class,
+    'controllers' => array(
+        'actions_category_ajax' => ActionsCategoryAjax::class,
+        'tile' => TileController::class,
+        'tile_list' => TileList::class,
+        'tile_main' => TileMain::class,
     ),
-    'events'        => array(
-        'onActivate'   => '\NatureAndStyle\CoreModule\Core\Events::onActivate',
+    'events' => array(
+        'onActivate' => '\NatureAndStyle\CoreModule\Core\Events::onActivate',
     ),
-    'blocks'        => array(
+    'blocks' => array(
         //Actions
         array(
-            'template'  => 'actions_main.tpl',
-            'block'     => 'admin_actions_main_product',
-            'file'      => '/Application/views/blocks/actions_main_extend.tpl'
+            'template' => 'actions_main.tpl',
+            'block' => 'admin_actions_main_product',
+            'file' => '/Application/views/blocks/actions_main_extend.tpl'
         ),
 
         //Article
         array(
-            'template'  => 'article_main.tpl',
-            'block'     => 'admin_article_main_extended',
-            'file'      => '/Application/views/blocks/article_main_extend.tpl',
+            'template' => 'article_main.tpl',
+            'block' => 'admin_article_main_extended',
+            'file' => '/Application/views/blocks/article_main_extend.tpl',
         ),
         array(
-            'template'  => 'article_extend.tpl',
-            'block'     => 'admin_article_extend_form',
-            'file'      => 'Application/views/blocks/article_extend_extend.tpl',
+            'template' => 'article_extend.tpl',
+            'block' => 'admin_article_extend_form',
+            'file' => 'Application/views/blocks/article_extend_extend.tpl',
         ),
         array(
-            'template'  => 'article_stock.tpl',
-            'block'     => 'admin_article_stock_form',
-            'file'      => '/Application/views/blocks/article_stock_extend.tpl'
+            'template' => 'article_stock.tpl',
+            'block' => 'admin_article_stock_form',
+            'file' => '/Application/views/blocks/article_stock_extend.tpl'
         ),
         array(
-            'template'  => 'article_pictures.tpl',
-            'block'     => 'admin_article_pictures_main',
-            'file'      => '/Application/views/blocks/article_pictures_extend.tpl',
+            'template' => 'article_pictures.tpl',
+            'block' => 'admin_article_pictures_main',
+            'file' => '/Application/views/blocks/article_pictures_extend.tpl',
         ),
         //Delivery
         array(
-            'template'  => 'delivery_main.tpl',
-            'block'     => 'admin_delivery_main_form',
-            'file'      => '/Application/views/blocks/delivery_main_extend.tpl',
+            'template' => 'delivery_main.tpl',
+            'block' => 'admin_delivery_main_form',
+            'file' => '/Application/views/blocks/delivery_main_extend.tpl',
         ),
 
         //Discount
         array(
-            'template'  => 'discount_main.tpl',
-            'block'     => 'admin_discount_main_form',
-            'file'      => '/Application/views/blocks/discount_main_extend.tpl',
+            'template' => 'discount_main.tpl',
+            'block' => 'admin_discount_main_form',
+            'file' => '/Application/views/blocks/discount_main_extend.tpl',
         ),
     ),
 );
