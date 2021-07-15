7984b6606d0b2c1033cd371e0b969aa517602154
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-07-15 16:52:10 +0200

    some tests

--- Application/Controller/Admin/ShopConfiguration.php
+++ Application/Controller/Admin/ShopConfiguration.php
@@ -32,7 +32,7 @@ class ShopConfiguration extends ShopConfiguration_parent
 
                 $sId = (string)$row[0];
                 $sTitle = (string)$row[1];
-                $blSelected = Registry::getConfig()->getConfigParam('nascargodelivery') == $sId;
+                $blSelected = strcmp((string)Registry::getConfig()->getConfigParam('nascargodelivery'), $sId) == 0;
 
                 $currentDelivery = array($sId, $sTitle, $blSelected);
                 $logger = Registry::getLogger();
