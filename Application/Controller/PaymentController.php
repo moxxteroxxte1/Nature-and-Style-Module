2156b334fd7503378d620388625cf64a73fcfbeb
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-08-06 21:10:27 +0200

    test

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -21,37 +21,4 @@ class PaymentController extends PaymentController_parent
 
     }
 
-    public function getPaymentList()
-    {
-        if ($this->_oPaymentList === null) {
-            $this->_oPaymentList = false;
-
-            $sActShipSet = Registry::getRequest()->getRequestEscapedParameter('sShipSet');
-            if (!$sActShipSet) {
-                $sActShipSet = Registry::getSession()->getVariable('sShipSet');
-            }
-
-            $session = \OxidEsales\Eshop\Core\Registry::getSession();
-            $oBasket = $session->getBasket();
-
-            // load sets, active set, and active set payment list
-            list($aAllSets, $sActShipSet, $aPaymentList) =
-               Registry::get(DeliverySetList::class)->getDeliverySetData($sActShipSet, $this->getUser(), $oBasket);
-
-            $oBasket->setShipping($sActShipSet);
-
-            // calculating payment expences for preview for each payment
-            $this->setValues($aPaymentList, $oBasket);
-            $this->_oPaymentList = $aPaymentList;
-
-            $user = $this->getUser();
-            $oxDeliverySetList = oxNew(DeliverSetList::class);
-            $aActiveSets = $oxDeliverySetList->getActiveDeliverySetList($user, $user->getUserCountry());#
-
-            $this->_aAllSets = $aActiveSets;
-        }
-
-        return $this->_oPaymentList;
-    }
-
 }
