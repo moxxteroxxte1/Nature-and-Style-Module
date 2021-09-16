4233b77ae4f2511a73847530e7d8ba1293588e4d
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-09-16 17:28:29 +0200

    some fixes

--- Application/Controller/StartController.php
+++ Application/Controller/StartController.php
@@ -32,7 +32,7 @@ class StartController extends StartController_parent
     public function getBargainShortDescription()
     {
         $oDb = DatabaseProvider::getDb();
-        $sQ = "SELECT oxlongdesc FROM oxactions WHERE oxid = oxbargain";
+        $sQ = "SELECT oxlongdesc FROM oxactions WHERE oxid = 'oxbargain'";
         $resultSet = $oDb->select($sQ);
 
         if ($resultSet && $resultSet->count() > 0) {
