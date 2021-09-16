9c475cb49ae9c92432289828ec4462890335a8f7
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-09-16 17:27:34 +0200

    some fixes

--- Application/Controller/StartController.php
+++ Application/Controller/StartController.php
@@ -7,6 +7,7 @@ namespace NatureAndStyle\CoreModule\Application\Controller;
 use OxidEsales\Eshop\Application\Model\Actions;
 use OxidEsales\Eshop\Application\Model\ActionList;
 use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\Eshop\Core\DatabaseProvider;
 
 class StartController extends StartController_parent
 {
@@ -15,7 +16,7 @@ class StartController extends StartController_parent
     {
         $oTilesList = null;
 
-        if(Registry::getConfig()->getConfigParam('bl_perfLoadAktion')){
+        if (Registry::getConfig()->getConfigParam('bl_perfLoadAktion')) {
             $oTilesList = oxNew(ActionList::class);
             $oTilesList->loadTiles();
         }
@@ -23,15 +24,48 @@ class StartController extends StartController_parent
         return $oTilesList;
     }
 
-    public function getShopUrl(){
+    public function getShopUrl()
+    {
         return Registry::getConfig()->getShopUrl();
     }
 
     public function getBargainShortDescription()
     {
-        $oAction = oxNew(Actions::class);
-        $oAction->load('oxbargain');
-        return $oAction->oxactions__oxlongdesc->value;
+        $oDb = DatabaseProvider::getDb();
+        $sQ = "SELECT oxlongdesc FROM oxactions WHERE oxid = oxbargain";
+        $resultSet = $oDb->select($sQ);
+
+        if ($resultSet && $resultSet->count() > 0) {
+            while (!$resultSet->EOF) {
+                $row = $resultSet->getFields();
+                return (string)$row[0];
+
+            }
+        }
+        return null;
     }
+    public function getAllDeliverys()
+    {
+        $data = [];
+
+        $oDb = DatabaseProvider::getDb();
+        $sQ = "select oxid, oxtitle from oxdelivery where oxid != {$oDb->quote($this->getEditObjectId())}";
+
+        $resultSet = $oDb->select($sQ);
+
+        if ($resultSet && $resultSet->count() > 0) {
+            while (!$resultSet->EOF) {
+                $row = $resultSet->getFields();
+
+                $id = (string)$row[0];
+                $title = (string)$row[1];
+
+                array_push($data, array($id, $title));
+                $resultSet->fetchRow();
+            }
+        }
+        return $resultSet;
+    }
+
 
 }
