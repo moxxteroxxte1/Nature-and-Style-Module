ed2be46fd4b2d3d38d72ad7bd4414425df1c9516
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-09-17 08:53:02 +0200

    some fixes

--- Core/Price.php
+++ Core/Price.php
@@ -7,4 +7,41 @@ namespace NatureAndStyle\CoreModule\Core;
 class Price extends Price_parent
 {
 
+    public function getPrice()
+    {
+        if($this->isNettoMode()){
+            return $this->getNettoPrice();
+        }else{
+            return $this->getBruttoPrice();
+        }
+    }
+
+    /**
+     * Returns brutto price
+     *
+     * @return double
+     */
+    public function getBruttoPrice()
+    {
+        if ($this->isNettoMode()) {
+            return \OxidEsales\Eshop\Core\Registry::getUtils()->fRound($this->getNettoPrice() + $this->getVatValue());
+        } else {
+            return \OxidEsales\Eshop\Core\Registry::getUtils()->fRound($this->_dBrutto);
+        }
+    }
+
+    /**
+     * Returns netto price
+     *
+     * @return double
+     */
+    public function getNettoPrice()
+    {
+        if ($this->isNettoMode()) {
+            return \OxidEsales\Eshop\Core\Registry::getUtils()->fRound($this->_dNetto);
+        } else {
+            return $this->getBruttoPrice() - $this->getVatValue();
+        }
+    }
+
 }
