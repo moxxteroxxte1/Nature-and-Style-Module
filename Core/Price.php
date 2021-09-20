c78790796ea3ae84cd9781f8f41c4e32b1cf8bea
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-09-20 19:36:43 +0200

    some tests

--- Core/Price.php
+++ Core/Price.php
@@ -44,30 +44,17 @@ class Price extends Price_parent
     {
         $logger = Registry::getLogger();
         if ($this->isNettoMode()) {
-            $logger->info("---NETTOMODE---");
-            $dNettoPrice = Registry::getUtils()->fRound($this->_dNetto);
-            $logger->info("NettoPrice: " . $dNettoPrice);
+            $dNettoPrice =$this->getNettoPrice();
             $dVatValue = $dNettoPrice * $this->getVat() / 100;
-            $logger->info("VatValue: " . $dVatValue);
             $dBruttoPrice = round($dNettoPrice + $dVatValue, 1, PHP_ROUND_HALF_UP);
-            $logger->info("BruttoPrice: " . $dBruttoPrice);
             $dVatValue = $dBruttoPrice - $dNettoPrice;
-            $logger->info("VatValue: " . $dVatValue);
             $this->_dBrutto = $dBruttoPrice;
-            $logger->info("---------------");
         } else {
-            $logger->info("---BRUTTOMODE---");
-            $dBruttoPrice = Registry::getUtils()->fRound($this->_dBrutto);
-            $logger->info("BruttoPrice: " . $dBruttoPrice);
+            $dBruttoPrice = $this->getBruttoPrice();
             $dVatValue = $dBruttoPrice * $this->getVat() / (100 + $this->getVat());
-            $logger->info("VatValue: " . $dVatValue);
             $dNettoPrice = $dBruttoPrice - $dVatValue;
-            $dNettoPrice = Registry::getUtils()->fRound($this->_dNetto);
             $dVatValue = round($dBruttoPrice, 1, PHP_ROUND_HALF_UP) - $dNettoPrice;
-            $logger->info("VatValue: " . $dVatValue);
             $this->_dBrutto = $dNettoPrice + $dVatValue;
-            $this->_dNetto = $dNettoPrice;
-            $logger->info("---------------");
         }
 
         return Registry::getUtils()->fRound($dVatValue);
