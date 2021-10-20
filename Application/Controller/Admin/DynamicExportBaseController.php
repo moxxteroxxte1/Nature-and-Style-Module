2a77b188352e8643b4338c159591c30dd94fb708
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-20 15:42:38 +0200

    some tests

--- Application/Controller/Admin/DynamicExportBaseController.php
+++ Application/Controller/Admin/DynamicExportBaseController.php
@@ -11,4 +11,43 @@ class DynamicExportBaseController extends DynamicExportBaseController_parent
 
     public $sExportFileType = "csv";
 
+    protected function insertArticles($sHeapTable, $sCatAdd)
+    {
+        $oDB = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
+
+        $iExpLang = Registry::getRequest()->getRequestEscapedParameter("iExportLanguage");
+        if (!isset($iExpLang)) {
+            $iExpLang = \OxidEsales\Eshop\Core\Registry::getSession()->getVariable("iExportLanguage");
+        }
+
+        $oArticle = oxNew(\OxidEsales\Eshop\Application\Model\Article::class);
+        $oArticle->setLanguage($iExpLang);
+
+        $tableViewNameGenerator = oxNew(TableViewNameGenerator::class);
+        $sArticleTable = $tableViewNameGenerator->getViewName("oxarticles", $iExpLang);
+
+        $insertQuery = "insert into {$sHeapTable} select {$sArticleTable}.oxid from {$sArticleTable} where ";
+
+        if (!Registry::getRequest()->getRequestEscapedParameter("blExportVars")) {
+            $insertQuery .= " and {$sArticleTable}.oxid = oxobject2category.oxobjectid and {$sArticleTable}.oxparentid = '' ";
+        } else {
+            $insertQuery .= " and ( {$sArticleTable}.oxid = oxobject2category.oxobjectid or {$sArticleTable}.oxparentid = oxobject2category.oxobjectid ) ";
+        }
+
+        $sSearchString = Registry::getRequest()->getRequestEscapedParameter("search");
+        if (isset($sSearchString)) {
+            $insertQuery .= "and ( {$sArticleTable}.OXTITLE like " . $oDB->quote("%{$sSearchString}%");
+            $insertQuery .= " or {$sArticleTable}.OXSHORTDESC like " . $oDB->quote("%$sSearchString%");
+            $insertQuery .= " or {$sArticleTable}.oxsearchkeys like " . $oDB->quote("%$sSearchString%") . " ) ";
+        }
+
+        if ($sCatAdd) {
+            $insertQuery .= $sCatAdd;
+        }
+
+        $insertQuery .= " group by {$sArticleTable}.oxid";
+
+        return $oDB->execute($insertQuery) ? true : false;
+    }
+
 }
