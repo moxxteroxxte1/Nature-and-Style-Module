8e75f528351ef6b53319ba0c1c2160d15a6bed63
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-21 11:52:59 +0200

    tests

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -12,57 +12,12 @@ class PaymentController extends PaymentController_parent
 
     public function render()
     {
-        $myConfig = \OxidEsales\Eshop\Core\Registry::getConfig();
-
-        if ($myConfig->getConfigParam('blPsBasketReservationEnabled')) {
-            $session = \OxidEsales\Eshop\Core\Registry::getSession();
-            $session->getBasketReservations()->renewExpiration();
-        }
-
         parent::render();
 
-        //if it happens that you are not in SSL
-        //then forcing to HTTPS
-
-        //but first checking maybe there were redirection already to prevent infinite redirections
-        //due to possible buggy ssl detection on server
-        $blAlreadyRedirected = Registry::getRequest()->getRequestEscapedParameter('sslredirect') == 'forced';
-
-        if ($this->getIsOrderStep()) {
-            //additional check if we really really have a user now
-            //and the basket is not empty
-            $session = \OxidEsales\Eshop\Core\Registry::getSession();
-            $oBasket = $session->getBasket();
-            $blPsBasketReservationEnabled = $myConfig->getConfigParam('blPsBasketReservationEnabled');
-            if ($blPsBasketReservationEnabled && (!$oBasket || ($oBasket && !$oBasket->getProductsCount()))) {
-                Registry::getUtils()->redirect($myConfig->getShopHomeUrl() . 'cl=basket', true, 302);
-            }
-
-            $oUser = $this->getUser();
-            if (!$oUser && ($oBasket && $oBasket->getProductsCount() > 0)) {
-                Registry::getUtils()->redirect($myConfig->getShopHomeUrl() . 'cl=basket', false, 302);
-            } elseif (!$oBasket || !$oUser || ($oBasket && !$oBasket->getProductsCount())) {
-                Registry::getUtils()->redirect($myConfig->getShopHomeUrl() . 'cl=start', false, 302);
-            }
-        }
-
-        $sFncParameter = Registry::getRequest()->getRequestEscapedParameter('fnc');
-        if ($myConfig->getCurrentShopURL() != $myConfig->getSSLShopURL() && !$blAlreadyRedirected && !$sFncParameter) {
-            $sPayErrorParameter = Registry::getRequest()->getRequestEscapedParameter('payerror');
-            $sPayErrorTextParameter = Registry::getRequest()->getRequestEscapedParameter('payerrortext');
-            $shopSecureHomeURL = $myConfig->getShopSecureHomeURL();
-
-            $sPayError = $sPayErrorParameter ? 'payerror=' . $sPayErrorParameter : '';
-            $sPayErrorText = $sPayErrorTextParameter ? 'payerrortext=' . $sPayErrorTextParameter : '';
-            $sRedirectURL = $shopSecureHomeURL . 'sslredirect=forced&cl=payment&' . $sPayError . "&" . $sPayErrorText;
-            Registry::getUtils()->redirect($sRedirectURL, true, 302);
-        }
-
         if (!$this->getAllSetsCnt()) {
+            unset($this->_oEmptyPayment);
             Registry::getSession()->setVariable('sShipSet', null);
         }
-
-        $this->unsetPaymentErrors();
 
         return $this->_sThisTemplate;
     }
