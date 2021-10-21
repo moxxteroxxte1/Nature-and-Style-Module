448cb52d5d17577ea4802d7d439b6417c4c15e4f
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-21 13:08:33 +0200

    tests

--- Application/Controller/Admin/GenericImportMain.php
+++ Application/Controller/Admin/GenericImportMain.php
@@ -11,6 +11,7 @@ class GenericImportMain extends GenericImportMain_parent
     public function render()
     {
         if (Registry::getRequest()->getRequestEscapedParameter('sNavStep') == 2) {
+            Registry::getLogger()->warning("2");
             $this->_aViewData['blAutoFillCSV'] =  Registry::getRequest()->getRequestEscapedParameter('blAutoFillCSV');
         }
         return parent::render();
