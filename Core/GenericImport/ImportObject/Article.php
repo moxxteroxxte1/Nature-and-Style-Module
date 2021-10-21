62939c8b5a1c6f3d1f86c37d4df322b8b23f7065
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-21 10:27:56 +0200

    tests

--- Core/GenericImport/ImportObject/Article.php
+++ Core/GenericImport/ImportObject/Article.php
@@ -3,6 +3,7 @@
 
 namespace NatureAndStyle\CoreModule\Core\GenericImport\ImportObject;
 
+use OxidEsales\Eshop\Core\Registry;
 
 class Article extends Article_parent
 {
@@ -10,6 +11,8 @@ class Article extends Article_parent
     public function getFieldList()
     {
         parent::getFieldList();
+        $logger = Registry::getLogger();
+        $logger->info(each($this->fieldList));
         array_push($this->fieldList , "OXLONGDESC");
         return $this->fieldList;
     }
