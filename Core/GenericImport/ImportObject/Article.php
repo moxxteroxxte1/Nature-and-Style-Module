e603fa93a04e143f5867b3cf6d5541826cb42161
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-21 10:24:40 +0200

    tests

--- Core/GenericImport/ImportObject/Article.php
+++ Core/GenericImport/ImportObject/Article.php
@@ -9,9 +9,9 @@ class Article extends Article_parent
 
     public function getFieldList()
     {
-        $fieldList = parent::getFieldList();
-        array_push($fieldList, "OXLONGDESC");
-        return $fieldList ;
+        parent::getFieldList();
+        array_push($this->fieldList , "OXLONGDESC");
+        return $this->fieldList;
     }
 
 }
