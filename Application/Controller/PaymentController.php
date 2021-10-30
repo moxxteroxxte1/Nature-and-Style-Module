54bb5d0ddedad2ca65e8a730bbff4812cb53c19b
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-30 13:47:24 +0200

    some fixes

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -130,7 +130,7 @@ class PaymentController extends PaymentController_parent
         if ($blOK) {
             $session->setVariable('paymentid', $sPaymentId);
             $session->setVariable('dynvalue', $aDynvalue);
-            $oBasket->setShipping($sShipSetId);
+            //$oBasket->setShipping($sShipSetId);
             $session->deleteVariable('_selected_paymentid');
 
             return 'order';
