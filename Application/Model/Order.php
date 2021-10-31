a0eee7844b884a86813b2bba1dc62547fd78fbfa
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-31 12:30:14 +0100

    tests

--- Application/Model/Order.php
+++ Application/Model/Order.php
@@ -9,10 +9,41 @@ class Order extends Order_parent
     public function validateDelivery($oBasket)
     {
         if(Registry::getSession()->getVariable('hasNoShipSet')){
-            Registry::getLogger()->error("noShipSet");
             return;
         }
         return parent::validateDelivery;
     }
 
+    public function validateOrder($oBasket, $oUser)
+    {
+        // validating stock
+        $iValidState = $this->validateStock($oBasket);
+
+        if (!$iValidState) {
+            // validating delivery
+            $iValidState = $this->validateDelivery($oBasket);
+        }
+
+        if (!$iValidState) {
+            // validating payment
+            $iValidState = $this->validatePayment($oBasket, $oUser);
+        }
+
+        if (!$iValidState) {
+            //0003110 validating delivery address, it is not be changed during checkout process
+            $iValidState = $this->validateDeliveryAddress($oUser);
+        }
+
+        if (!$iValidState) {
+            // validating minimum price
+            $iValidState = $this->validateBasket($oBasket);
+        }
+
+        if (!$iValidState) {
+            // validating vouchers
+            $iValidState = $this->validateVouchers($oBasket);
+        }
+
+        return $iValidState;
+    }
 }
