accce022991bf41c8072bfb6d085e1d8f2bae1fd
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-31 12:31:27 +0100

    tests

--- Application/Model/Order.php
+++ Application/Model/Order.php
@@ -9,7 +9,7 @@ class Order extends Order_parent
     public function validateDelivery($oBasket)
     {
         if(Registry::getSession()->getVariable('hasNoShipSet')){
-            return;
+            return true;
         }
         return parent::validateDelivery;
     }
