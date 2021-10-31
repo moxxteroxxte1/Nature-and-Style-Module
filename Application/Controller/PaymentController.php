9b5be4d776a8458cc0673e9e554dc9c17472fb21
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-10-31 11:41:09 +0100

    tests

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -127,17 +127,18 @@ class PaymentController extends PaymentController_parent
 
         $blOK = $oPayment->isValidPayment($aDynvalue, $myConfig->getShopId(), $oUser, $dBasketPrice, $sShipSetId);
 
-        Registry::getLogger()->error($blOK);
-
         if ($blOK) {
+            $logger = Registry::getLogger();
             $session->setVariable('paymentid', $sPaymentId);
             $session->setVariable('dynvalue', $aDynvalue);
+            $logger->error(1);
             $sShipSetId = ($session->getVariable('hasNoShipSet') ? null : $sShipSetId);
             $oBasket->setShipping($sShipSetId);
+            $logger->error(2);
             $session->deleteVariable('_selected_paymentid');
             $session->deleteVariable('sActShipSet');
             $session->deleteVariable('hasNoShipSet');
-
+            $logger->error(3);
             return 'order';
         } else {
             $session->setVariable('payerror', $oPayment->getPaymentErrorNumber());
