e4eb31075128c4cfddbf10fd4a538b1fcc02b125
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-11-08 15:53:47 +0100

    some fixes

--- Application/Model/User.php
+++ Application/Model/User.php
@@ -4,6 +4,7 @@ namespace NatureAndStyle\CoreModule\Application\Model;
 
 use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\Eshop\Core\DatabaseProvider;
+use OxidEsales\Eshop\Core\Database\Adapter\DatabaseInterface;
 
 class User extends User_parent
 {
