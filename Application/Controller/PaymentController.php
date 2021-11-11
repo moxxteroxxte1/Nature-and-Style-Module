32aa4b37ae78e319a0424aadccc2fe44aca3310a
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-11-11 18:27:52 +0100

    added support for Telephone notification cost by Deliverysets

--- Application/Controller/PaymentController.php
+++ Application/Controller/PaymentController.php
@@ -23,6 +23,7 @@ class PaymentController extends PaymentController_parent
         $oBasket->setFindCheapest(false);
         $oBasket->onUpdate();
         $oBasket->setShipping('sShipSet', Registry::getRequest()->getRequestEscapedParameter('sShipSet'));
+        Registry::getLogger()->warning('changeshipping: ' . boolval(Registry::getRequest()->getRequestEscapedParameter('blTelAvis')));
         $oBasket->handleTelAvis(boolval(Registry::getRequest()->getRequestEscapedParameter('blTelAvis')));
     }
 
