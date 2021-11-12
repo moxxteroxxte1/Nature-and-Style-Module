5a681fced7945fe9136bf54fa050df2980e56c9b
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-11-12 14:45:46 +0100

    added sendRegisterEmailToOwner() function to Email.php

--- Application/Component/UserComponent.php
+++ Application/Component/UserComponent.php
@@ -3,6 +3,7 @@
 namespace NatureAndStyle\CoreModule\Application\Component;
 
 use NatureAndStyle\CoreModule\Core\Email;
+use OxidEsales\Eshop\Core\Field;
 use OxidEsales\Eshop\Core\Registry;
 
 class UserComponent extends UserComponent_parent
@@ -12,12 +13,12 @@ class UserComponent extends UserComponent_parent
         if(parent::createUser() !== false)
         {
             $oUser = $this->getUser();
-            $oUser->oxuser__oxactive = new \OxidEsales\Eshop\Core\Field(0);
-            $oUser->save();
 
             $oxEMail = oxNew(Email::class);
             $oxEMail->sendRegisterEmailToOwner($oUser);
 
+            $oUser->oxuser__oxactive = new Field(0);
+            $oUser->save();
             $oUser->logout();
 
             $sUrl = Registry::getConfig()->getShopHomeUrl() . 'cl=content&tpl=user_inactive.tpl';
