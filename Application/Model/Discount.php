a0b16ca5b3e4391964e772f48718c48f81336a74
moxxteroxxte1 <koctiD-rivci1-cuwjed>
2021-11-25 01:13:38 +0100

    updated getDiscounts() in Article.php

--- Application/Model/Discount.php
+++ Application/Model/Discount.php
@@ -44,5 +44,55 @@ class Discount extends Discount_parent
     {
         return ($this->isArticleAssigned($this) || $this->isCategoriesAssigned($this->getCategoryIds()));
     }
+
+    protected function isArticleAssigned($oArticle)
+    {
+        $oDb = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
+
+        $sQ = "select 1
+                from oxobject2discount
+                where oxdiscountid = :oxdiscountid 
+                    and oxtype = :oxtype ";
+        $sQ .= $this->getProductCheckQuery($oArticle);
+        $params = [
+            ':oxdiscountid' => $this->oxdiscount__oxid->value,
+            ':oxtype' => 'oxarticles'
+        ];
+
+        return $oDb->getOne($sQ, $params) ? true : false;
+    }
+
+    protected function getProductCheckQuery($oProduct)
+    {
+        $oDb = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
+        // check if this article is assigned
+        if (($sParentId = $oProduct->getParentId())) {
+            $sArticleId = " and ( oxobjectid = " . $oDb->quote($oProduct->getProductId()) . " or oxobjectid = " . $oDb->quote($sParentId) . " )";
+        } else {
+            $sArticleId = " and oxobjectid = " . $oDb->quote($oProduct->getProductId());
+        }
+
+        return $sArticleId;
+    }
+
+    protected function isCategoriesAssigned($aCategoryIds)
+    {
+        if (empty($aCategoryIds)) {
+            return false;
+        }
+
+        $oDb = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
+
+        $sCategoryIds = "(" . implode(",", \OxidEsales\Eshop\Core\DatabaseProvider::getDb()->quoteArray($aCategoryIds)) . ")";
+        $sQ = "select 1
+                from oxobject2discount
+                where oxdiscountid = :oxdiscountid and oxobjectid in {$sCategoryIds} and oxtype = :oxtype";
+        $params = [
+            ':oxdiscountid' => $this->oxdiscount__oxid->value,
+            ':oxtype' => 'oxcategories'
+        ];
+
+        return $oDb->getOne($sQ, $params) ? true : false;
+    }
 }
 
